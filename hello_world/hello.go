d3bbecf9a3f3d3c8e6aeeb2ac17e377f42b5f15d
bodowd <odowd.bing@gmail.com>
2022-04-26 21:04:08 +0200

    added french and spanish

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+// public functions start with capital letter and private ones start with lowercase
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	// named return value (prefix string) and can just return whatever it's set to
+	// by just calling return
+	return
+
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 
 }
