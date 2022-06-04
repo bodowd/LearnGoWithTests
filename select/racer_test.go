aaebfcb9a2d2c530ff23dbb9185290d214647edf
bodowd <odowd.bing@gmail.com>
2022-06-04 14:00:47 +0200

    finished select

--- select/racer_test.go
+++ select/racer_test.go
@@ -9,23 +9,40 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(25 * time.Millisecond)
 
-	// will run this at the end of the function, but we want to keep it close to near where the server
-	// was created so that it is easier for future readers of the code
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer serverA.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		_, err := ConfigurableRacer(serverA.URL, serverA.URL, 20*time.Millisecond)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	})
+
+	t.Run("normal test", func(t *testing.T) {
+
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		// will run this at the end of the function, but we want to keep it close to near where the server
+		// was created so that it is easier for future readers of the code
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
 
 }
 
