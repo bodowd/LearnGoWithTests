a8dfcf93cdb862c87de466fd237aabb650c609c0
bodowd <odowd.bing@gmail.com>
2022-12-04 18:43:32 +0100

    Expand and refactor

--- 1_HelloWorld/hello_test.go
+++ 1_HelloWorld/hello_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// testing.TB is an interface so that you can call helper functions from a test
+// or benchmark
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	// this is needed to tell the test suite that this method is a helper
+	// this will make the line number reported when a test fails be from our
+	// function call rather than inside our test helper
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
