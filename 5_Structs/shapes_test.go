d43639415b38982c28caa46077d98393eee99ea0
bodowd <odowd.bing@gmail.com>
2022-12-08 14:26:22 +0100

    Finish section

--- 5_Structs/shapes_test.go
+++ 5_Structs/shapes_test.go
@@ -14,25 +14,28 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	type AreaTestsInput struct {
+		name  string
+		shape Shape
+		want  float64
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
+	areaTests := []AreaTestsInput{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
 
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the t.Run test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
 
-	})
+			}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-
-	})
+		})
+	}
 
 }
