32c9b8e6375bdfe1c2ba03ad5895fab7a2d4ac83
bodowd <odowd.bing@gmail.com>
2022-12-08 12:02:20 +0100

    Finished arrays and slices section

--- 4_ArraysAndSlices/sum_test.go
+++ 4_ArraysAndSlices/sum_test.go
@@ -23,7 +23,42 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
+	assertCorrectDeepEqual(t, got, want)
+}
 
+func TestSumAllTails(t *testing.T) {
+
+	// new technique, assign function to variable
+	// useful when you want to bind a function to other local variables in "scope"
+	// also reduces the surface area of your API
+	checkSums := func(t testing.TB, got, want []int) {
+
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
+
+func assertCorrectDeepEqual(t testing.TB, got []int, want []int) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
