239a3718615eb608ae12987d16b3f7217bb43f2f
bodowd <odowd.bing@gmail.com>
2022-12-08 11:44:52 +0100

    Second iteration

--- 4_ArraysAndSlices/sum.go
+++ 4_ArraysAndSlices/sum.go
@@ -9,3 +9,16 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	numberOfSlices := len(numbersToSum)
+	// create a slice with a starting capacity of the len of the nuberOfSlices we need to work through
+	sums := make([]int, numberOfSlices)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+
+}
