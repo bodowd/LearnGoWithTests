7e9b3e9055de64aeec240762cdbb10a4d6272bf8
bodowd <odowd.bing@gmail.com>
2022-12-10 21:53:33 +0100

    Fix second test case

--- 12_Reflection/walk.go
+++ 12_Reflection/walk.go
@@ -5,8 +5,12 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	// we make assumptions here about the value passed in
 	val := reflect.ValueOf(x)
-	// look at the first field. Could be that there are no fields at all, which would cause a panic
-	field := val.Field(0)
-	// call String() but it might not have this method
-	fn(field.String())
+
+	// iterate through each field in val
+	for i := 0; i < val.NumField(); i++ {
+		// access Field i
+		field := val.Field(i)
+		// call the String method. May not actually have one if it's not a string
+		fn(field.String())
+	}
 }
