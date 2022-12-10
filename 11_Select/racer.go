c4cdde9e4927afa7356656d00f239d4ceb65899e
bodowd <odowd.bing@gmail.com>
2022-12-10 20:38:58 +0100

    Finish section

--- 11_Select/racer.go
+++ 11_Select/racer.go
@@ -1,19 +1,33 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(a, b string) (winner string) {
+var tenSecondTimeout = 10 * time.Second
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	// select lets you wait on multiple channels
 	// the first one to send a value "wins" and the code underneath the case is executed
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+
+		// good to have a case where a signal is sent to select incase the code above
+		// blocks forever because the channels you're listening on never return a value
+
+		// time.After also returns a channel so we can use it here like so
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 
+}
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
 func ping(url string) chan struct{} {
