2026d5192c959a8344962f2fd05fbe2e244cbde0
bodowd <odowd.bing@gmail.com>
2022-12-26 14:42:17 +0100

    Refactor League

--- app/file_system_store.go
+++ app/file_system_store.go
@@ -9,35 +9,27 @@ type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
 }
 
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+	if player != nil {
+		return player.Wins
 	}
-	return wins
+	return 0
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
 
-	for i, player := range league {
-		if player.Name == name {
-			// needs to be this way instead of player.Wins++
-			// when you range over a slice you are returned the current index
-			// and a COPY of the element at that index
-			// so player.Wins++ modifies a copy, not the underlying
-			// slice that we are iterating on
-			league[i].Wins++
-		}
+	player := league.Find(name)
+
+	if player != nil {
+		player.Wins++
 	}
 
 	f.database.Seek(0, 0)
