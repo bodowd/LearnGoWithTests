b6aa58e02f4ede5c08ea36aae348e6fd08128bfd
bodowd <odowd.bing@gmail.com>
2022-12-26 17:54:43 +0100

    Cleaup constructor

--- app/file_system_store.go
+++ app/file_system_store.go
@@ -12,21 +12,18 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-	info, err := file.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
+	err := initialisePlayerDBFile(file)
 
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+	if err != nil {
+		return nil, fmt.Errorf("problem initializing player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
+
 	if err != nil {
 		return nil, fmt.Errorf("Problem loading store from file %s, %v", file.Name(), err)
 	}
+
 	return &FileSystemPlayerStore{
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
@@ -56,4 +53,18 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	}
 
 	f.database.Encode(f.league)
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+	return nil
 }
